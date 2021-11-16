754c5a406a4184a8c31dd6b8f4feb36355919e14
Sergio A. Martínez Fernández <salberto.mtz@gmail.com>
2021-11-16 00:05:58 -0600

    Adding env var to choose protocol

--- src/diabetes_hydra_service/config.clj
+++ src/diabetes_hydra_service/config.clj
@@ -1,9 +1,10 @@
 (ns diabetes-hydra-service.config
   (:require [levanzo.http :as http]
-            [levanzo.payload :as payload]
+            [levanzo.payload :as payload]            
             [clojure.string :as string]))
 
-
+(def protocol (or (System/getenv "PROTOCOL") "http"))
 (def port (Integer/parseInt (or (System/getenv "PORT") "8080")))
 (def host (or (System/getenv "HOST") "localhost"))
-(def base (str "http://" host ":" port "/"))
+(def base (str protocol "://" host ":" port "/"))
+(def prod-base (or (System/getenv "PROD_BASE") base))
